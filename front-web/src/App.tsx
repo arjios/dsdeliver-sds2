0a46672f55f7f322d4093b926d74895d8f5306b0
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-01-06 12:22:10 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div >
-      Hello World!!!
+      <Navbar />
     </div>
   );
 }
