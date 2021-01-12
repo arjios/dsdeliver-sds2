44331565cd9f284c7fc9ec552d37123382888b31
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-01-12 11:04:54 -0300

    Add Home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import Header from './src/Header';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import Home from './src/Home';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular, 
+    OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <Header />
       <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
