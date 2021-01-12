544dc12d10583f6ed97ffd5ad2570137b37b9fc1
Arjios <antonio.uab.ios.fisica@gmail.com>
2021-01-12 09:26:16 -0300

    Add front-mobile initial files

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
 
-export default function App() {
+export default function Header() {
   return (
     <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />
+      <Text>DS Delivery</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: '#DA5C5C';
   },
 });
